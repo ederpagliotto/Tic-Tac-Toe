593b1ea661fbb194f0e2f0415241d7b59c087b9a
Eder Pagliotto <ederpagliotto@gmail.com>
2022-10-24 18:47:43 +0100

    added createPlayer factory function

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 // Implementing gameboard module
 const gameBoardModule = (function() {
     let gameBoard = [];
-    return {};
-})();
+    return {};   //return an object or a function. (turns public)
+})();            // () invoke the function immediately
+
+
+const displayControllerModule = (function() {
+    let testFunction = () => {console.log("Testing private function call inside of a module object...... ")};
+    return {testFunction}; //testing if return as a public
+})
+
+//player factory function, looks similar but it doesn't have any parentesis to invoke function instantly
+let createPlayer = (playerName, playerNumber, assignedxO) => {
+    let getPlayerName = () => { playerName;
+    console.log("This is the name of player " + playerNumber + "......." + playerName); }
+    return {getPlayerName, playerName, playerNumber, assignedxO};
+};
