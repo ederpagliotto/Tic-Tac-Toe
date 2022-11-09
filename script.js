5ae359051e846c3397d44aa5df7b9dcb92b713b1
Eder Pagliotto <ederpagliotto@gmail.com>
2022-11-09 20:05:44 +0000

    implementing function to check Winner

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
-const celulas = document.querySelectorAll(".celula");
+const cells = document.querySelectorAll(".cell");
 const playerX = "X";
 const playerO = "O";
 let playerTurn = true;
 
+// Implementing gameboard module
+/*const gameBoardModule = (function() {
+    let gameBoard = [];
+    return {};   //return an object or a function. (turns public)
+})();            // () invoke the function immediately
+*/
+
+const combinations = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+
 document.addEventListener("click", (event) => {
-    if (event.target.matches(".celula")) {
+    if (event.target.matches(".cell")) {
         console.log(event.target.id);
         play(event.target.id);
     }
 });
 
 function play(id) {
-    const celula = document.getElementById(id);
+    const cell = document.getElementById(id);
     turn = playerTurn ? playerO : playerX;       //ternary, if it's true playerO plays, if it's false playerx plays.
-    celula.textContent = turn;
+    cell.textContent = turn;
+    cell.classList.add(turn);
     playerTurn = !playerTurn;     // if the value is true, change to false and vice-versa.
 
    /* if (playerTurn == true) {
@@ -26,9 +45,10 @@ function play(id) {
 }
 
 
-// Implementing gameboard module
-/*const gameBoardModule = (function() {
-    let gameBoard = [];
-    return {};   //return an object or a function. (turns public)
-})();            // () invoke the function immediately
-*/
+function checkWinner(turn) {
+    const winner = combinations.some((comb) => {          // if some item in array returns true all the function returns true
+        return comb.every((index) => {
+
+        })       //all combinations needs to be true to return true, if one of them be false returns false
+    })
+}
